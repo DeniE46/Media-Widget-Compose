76e5807e86dea53faa3e1cd9479cf297ef35742b
DeniE46 <denislavpopov46@gmail.com>
2024-02-09 15:48:36 +0200

    * Introduce drawables from original MediaWidget * Rework the UI to replace buttons with boxes and use them instead * Switch to default default MediaWidget background 9patch plate * implement music, photo and video icons

--- app/src/main/java/com/example/axpmediawidget/MediaWidget.kt
+++ app/src/main/java/com/example/axpmediawidget/MediaWidget.kt
@@ -1,15 +1,26 @@
 package com.example.axpmediawidget
 
 import android.content.Context
+import android.content.res.loader.ResourcesProvider
 import android.util.Log
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme.colors
+import androidx.glance.ImageProvider
 import androidx.glance.action.ActionParameters
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.SizeMode
@@ -18,9 +29,12 @@ import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
 import androidx.glance.layout.Row
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.height
 import androidx.glance.layout.padding
+import androidx.glance.layout.width
 import androidx.glance.state.GlanceStateDefinition
 
 object MediaWidget: GlanceAppWidget() {
@@ -48,27 +62,27 @@ private fun Widget() {
     Row(
         modifier = GlanceModifier
             .fillMaxSize()
-            .background(Color.DarkGray)
-            .padding(5.dp),
+            .background(ImageProvider(resId = R.drawable.widget_plate)),
         verticalAlignment = Alignment.Vertical.CenterVertically,
         horizontalAlignment = Alignment.Horizontal.CenterHorizontally
     ) {
         val modifier = GlanceModifier.defaultWeight()
-        Button(
-            text = "click",
-            onClick = actionRunCallback(ClickActionCallback()::class.java),
-            modifier
+        Box (
+            modifier.background(ImageProvider(resId = R.drawable.music_button))
+                .clickable(onClick = actionRunCallback(ClickActionCallback()::class.java))
         )
-        Button(
-            text = "click",
-            onClick = actionRunCallback(ClickActionCallback()::class.java),
-            modifier
+        {}
+        Box (
+            modifier.background(ImageProvider(resId = R.drawable.photo_button))
+                .clickable(onClick = actionRunCallback(ClickActionCallback()::class.java))
         )
-        Button(
-            text = "click",
-            onClick = actionRunCallback(ClickActionCallback()::class.java),
-            modifier
+        {}
+        Box (
+            modifier.background(ImageProvider(resId = R.drawable.video_button))
+                .clickable(onClick = actionRunCallback(ClickActionCallback()::class.java))
         )
+        {}
+
     }
 }
 
